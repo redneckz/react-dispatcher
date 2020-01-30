61254d295eac7c5b5250f080e2253454977c78ca
pavyarov <Pavel_Iarov@epam.com>
2020-01-30 14:06:34 +0300

    Fix TS defenitions for createDispatcher, add defenitions for applyMiddleware

--- react-dispatcher.d.ts
+++ react-dispatcher.d.ts
@@ -1,5 +1,6 @@
 import * as React from 'react';
 
 declare module '@redneckz/react-dispatcher' {
-  export function createDispatcher(): <D extends React.Dispatch<any>>(state: any, dispatch: D) => D;
+  export function createDispatcher(): <D extends React.Dispatch<any>>(state?: any, dispatch?: D) => D;
+  export function applyMiddleware(...arg: Function[]): <D extends React.Dispatch<any>>(arg: (state?: any, dispatch?: D) => D) => void;
 }
